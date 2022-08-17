c63c20387f4a3480081766cabf0731c27ab5af61
Yofingert <dofingert@gmail.com>
2022-08-17 23:01:28 +0800

    加速

--- src/myhal.c
+++ src/myhal.c
@@ -63,7 +63,7 @@ void nes_set_bg_color(int c)
 {
     //fb_flip_display();
     printf("start nes_set_bg_color...  ");
-    // int x, y;
+    int x, y;
     BG_COLOR = color_map[c];
     for (y = 0; y < SCREEN_HEIGHT; ++y)
     {
